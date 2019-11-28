696b464fa8f029b17384eed2dcfb24e6a09bab77
Clouds42 <1577338630@qq.com>
2019-11-28 19:33:21 +0800

    V1.0.1

--- PWM.v
+++ PWM.v
@@ -1,7 +1,7 @@
 module PWM
 (input CLK
 ,input FLAG
-,input _OE
+,input BP
 ,output STT
 ,output PWM
 );
@@ -10,24 +10,38 @@ reg[24:0]cnt1;//Max=33554431
 reg[24:0]cnt2;
 reg stt;
 
-parameter FREQ=3464;//2*(FREQ)^2=24M=2s
+reg[24:0]FREQ=25'd1500;
+reg[1:0]f;
+always@(posedge BP)
+	f<=f+1'b1;
+always@(posedge CLK)
+	case(f)
+		2'b00:FREQ<=25'd1500;
+		2'b01:FREQ<=25'd3000;
+		2'b10:FREQ<=25'd4500;
+		default:FREQ<=25'd6000;
+	endcase
 
 always@(posedge CLK)
-	if(cnt1>=FREQ)
+	if(cnt1>=FREQ-1'b1)
 		cnt1<=1'b0;
 	else
 		cnt1<=cnt1+1'b1;
 
 always@(posedge CLK)
-	if(cnt1==FREQ)
+	if(cnt2==FREQ-1'b1)
+		stt<=1'b1;
+	else
+		stt<=1'b0;
+
+always@(posedge CLK)
+	if(cnt1==FREQ-1'b1)
 		if(FLAG)
-			if(cnt2>=FREQ)begin
-				cnt2<=1'b0;stt<=1'b1;
-			end
-			else begin
-				cnt2<=cnt2+1'b1;stt<=1'b0;
-			end
-		else
+			if(cnt2>=FREQ-1'b1)
+				cnt2<=1'b0;
+			else
+				cnt2<=cnt2+1'b1;
+		else if(!FLAG)
 			if(cnt2<=0)
 				cnt2<=FREQ-1'b1;
 			else
@@ -35,7 +49,7 @@ always@(posedge CLK)
 	else
 		cnt2<=cnt2;
 
-assign PWM=(_OE==1)?((cnt1<cnt2)?1'b0:1'b1):1'b1;//核心
+assign PWM=(cnt1<cnt2)?1'b1:1'b0;//核心
 assign STT=stt;
 
 endmodule
--- RainbowRGB.v
+++ RainbowRGB.v
@@ -5,18 +5,21 @@
 */
 module RainbowRGB
 (input CLK
+,input BTN
 ,output[2:0]LED
 );
 
-wire UP;
+wire BP;
+Debounce UUD4(CLK,BTN,BP);
+
+reg[23:0]freq;
 wire DW;
-wire NO;
+wire UP;
 wire STT1;
 wire STT2;//占位
 wire STT3;//占位
-PWM UUD1(CLK,1,1,STT1,UP);
-PWM UUD2(CLK,0,1,STT2,DW);
-PWM UUD3(CLK,1,0,STT3,NO);
+PWM UUD1(CLK,1,BP,STT1,DW);
+PWM UUD2(CLK,0,BP,STT2,UP);
 
 reg[1:0]flag1=2'b00;
 reg[1:0]flag2=2'b01;
@@ -37,8 +40,45 @@ always@(posedge STT1)
 	else
 		flag3<=flag3+1'b1;
 
-assign LED[0]=(flag1==2'b00)?UP:((flag1==2'b01)?DW:NO);
-assign LED[1]=(flag2==2'b00)?UP:((flag2==2'b01)?DW:NO);
-assign LED[2]=(flag3==2'b00)?UP:((flag3==2'b01)?DW:NO);
+assign LED[0]=(flag1==2'b00)?UP:((flag1==2'b01)?DW:1'b1);
+//assign LED[1]=(flag2==2'b00)?DW:((flag2==2'b01)?UP:1'b1);
+//assign LED[2]=(flag3==2'b00)?DW:((flag3==2'b01)?UP:1'b1);
+//assign LED[0]=UP;
+assign LED[1]=1'b1;
+assign LED[2]=1'b1;
+
+endmodule
+
+module Debounce
+(input CLK
+,input KEY//待消抖按键
+,output KP//消抖后的按键脉冲
+);
+
+reg[18:0]cnt_h;//高电平计数器
+reg[18:0]cnt_l;//低电平计数器
+reg kp;
+
+always @(posedge CLK)//按键为低电平时,开始计数
+	if(!KEY)
+		cnt_l<=cnt_l+1'b1;
+	else
+		cnt_l<=0;
+
+always@(posedge CLK)//按键为高电平时,开始计数
+	if(KEY)
+		cnt_h<=cnt_h+1'b1;
+	else
+		cnt_h<= 0;
+
+always@(posedge CLK)//计数器积攒到10ms发出按键脉冲
+	if(cnt_h==19'h7a120)
+		kp<=1;
+	else if(cnt_l==19'h7a120)
+		kp<=0;
+	else
+		kp<=kp;
+
+assign KP=kp;
 
 endmodule
