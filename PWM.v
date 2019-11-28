045d7dd644f77713d5f19121e612da7fe84a7cc7
Clouds42 <1577338630@qq.com>
2019-11-28 16:55:43 +0800

    V1.0

--- PWM.v
+++ PWM.v
@@ -1,44 +1,41 @@
 module PWM
 (input CLK
-,input RST
+,input FLAG
+,input _OE
+,output STT
 ,output PWM
 );
 
-reg[24:0]cnt1;
+reg[24:0]cnt1;//Max=33554431
 reg[24:0]cnt2;
-reg flag;
+reg stt;
 
-parameter FREQ=2450;
+parameter FREQ=3464;//2*(FREQ)^2=24M=2s
 
-always@(posedge CLK or negedge RST)
-	if(!RST)
-		cnt1<=13'd0;
+always@(posedge CLK)
+	if(cnt1>=FREQ)
+		cnt1<=1'b0;
 	else
-		if(cnt1>=FREQ-1'b1)
-			cnt1<=1'b0;
+		cnt1<=cnt1+1'b1;
+
+always@(posedge CLK)
+	if(cnt1==FREQ)
+		if(FLAG)
+			if(cnt2>=FREQ)begin
+				cnt2<=1'b0;stt<=1'b1;
+			end
+			else begin
+				cnt2<=cnt2+1'b1;stt<=1'b0;
+			end
 		else
-			cnt1<=cnt1+1'b1;
-
-always@(posedge CLK or negedge RST)
-	if(!RST)begin
-		cnt2<=13'd0;
-		flag<=1'b0;
-	end
-	else
-		if(cnt1==FREQ-1)
-			if(!flag)
-				if(cnt2>=FREQ-1)
-					flag<=1'b1;
-				else
-					cnt2<=cnt2+1'b1;
+			if(cnt2<=0)
+				cnt2<=FREQ-1'b1;
 			else
-				if(cnt2<=0)
-					flag<=1'b0;
-				else
-					cnt2<=cnt2-1'b1;
-		else
-			cnt2<=cnt2;
+				cnt2<=cnt2-1'b1;
+	else
+		cnt2<=cnt2;
 
-assign PWM=(cnt1<cnt2)?1'b0:1'b1;//核心
+assign PWM=(_OE==1)?((cnt1<cnt2)?1'b0:1'b1):1'b1;//核心
+assign STT=stt;
 
 endmodule
--- RainbowRGB.v
+++ RainbowRGB.v
@@ -5,10 +5,40 @@
 */
 module RainbowRGB
 (input CLK
-,input BTN_RST
-,output[2:0]RGB
+,output[2:0]LED
 );
 
+wire UP;
+wire DW;
+wire NO;
+wire STT1;
+wire STT2;//占位
+wire STT3;//占位
+PWM UUD1(CLK,1,1,STT1,UP);
+PWM UUD2(CLK,0,1,STT2,DW);
+PWM UUD3(CLK,1,0,STT3,NO);
 
+reg[1:0]flag1=2'b00;
+reg[1:0]flag2=2'b01;
+reg[1:0]flag3=2'b10;
+always@(posedge STT1)
+	if(flag1==2'b10)
+		flag1<=2'b00;
+	else
+		flag1<=flag1+1'b1;
+always@(posedge STT1)
+	if(flag2==2'b10)
+		flag2<=2'b00;
+	else
+		flag2<=flag2+1'b1;
+always@(posedge STT1)
+	if(flag3==2'b10)
+		flag3<=2'b00;
+	else
+		flag3<=flag3+1'b1;
+
+assign LED[0]=(flag1==2'b00)?UP:((flag1==2'b01)?DW:NO);
+assign LED[1]=(flag2==2'b00)?UP:((flag2==2'b01)?DW:NO);
+assign LED[2]=(flag3==2'b00)?UP:((flag3==2'b01)?DW:NO);
 
 endmodule
