d339ea02f73dd9d87e4b6f3d83845153570f0026
Clouds42 <1577338630@qq.com>
2019-11-28 20:31:45 +0800

    V2.0

--- PWM.v
+++ PWM.v
@@ -1,30 +1,39 @@
 module PWM
 (input CLK
+,input FLAG
+,output STT
 ,output PWM
 );
 
 reg[24:0]cnt1;
 reg[24:0]cnt2;
-reg flag;
 
 parameter FREQ=2400;
 
+reg stt;
 always@(posedge CLK)
-	if(cnt1>=FREQ-1) 
+	if(cnt2==FREQ-1)
+		stt<=1'b1;
+	else
+		stt<=1'b0;
+assign STT=stt;
+
+always@(posedge CLK)
+	if(cnt1>=FREQ-1)
 		cnt1<=1'b0;
 	else 
-		cnt1<=cnt1+1'b1; 
+		cnt1<=cnt1+1'b1;
 
 always@(posedge CLK)
 	if(cnt1==FREQ-1)
-		if(!flag)
+		if(FLAG)
 			if(cnt2>=FREQ-1)
-				flag<=1'b1;
+				cnt2<=1'b0;
 			else
 				cnt2<=cnt2+1'b1;
 		else
 			if(cnt2<=0)
-				flag<=1'b0;
+				cnt2<=FREQ-1;
 			else 
 				cnt2<=cnt2-1'b1;
 	else
--- RainbowRGB.v
+++ RainbowRGB.v
@@ -1,51 +1,22 @@
-/*
-实现彩虹效果呼吸灯,
-用按键控制呼吸频率,
-单灯或双灯同步等功能.
-*/
 module RainbowRGB
 (input CLK
 ,output LED
 );
 
-wire PWM;
-PWM UUD(CLK,PWM);
-assign LED=PWM;
+wire UP;
+wire DW;
+wire STT0;
+wire STT1;
+PWM up(CLK,1,STT0,UP);
+PWM dw(CLK,0,STT1,DW);
 
-endmodule
-
-
-
-module Debounce
-(input CLK
-,input KEY//待消抖按键
-,output KP//消抖后的按键脉冲
-);
-
-reg[18:0]cnt_h;//高电平计数器
-reg[18:0]cnt_l;//低电平计数器
-reg kp;
-
-always @(posedge CLK)//按键为低电平时,开始计数
-	if(!KEY)
-		cnt_l<=cnt_l+1'b1;
+reg[1:0]flag;
+always@(posedge STT0)
+	if(flag==2'b10)
+		flag<=1'b0;
 	else
-		cnt_l<=0;
+		flag<=flag+1'b1;
 
-always@(posedge CLK)//按键为高电平时,开始计数
-	if(KEY)
-		cnt_h<=cnt_h+1'b1;
-	else
-		cnt_h<= 0;
-
-always@(posedge CLK)//计数器积攒到10ms发出按键脉冲
-	if(cnt_h==19'h7a120)
-		kp<=1;
-	else if(cnt_l==19'h7a120)
-		kp<=0;
-	else
-		kp<=kp;
-
-assign KP=kp;
+assign LED=(flag==2'b00)?UP:((flag==2'b01)?DW:1'b1);
 
 endmodule
