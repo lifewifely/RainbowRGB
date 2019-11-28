355a11406b424d919b345b52d36a55685fef1239
Clouds42 <1577338630@qq.com>
2019-11-28 20:37:54 +0800

    V2.1

--- RainbowRGB.v
+++ RainbowRGB.v
@@ -1,6 +1,6 @@
 module RainbowRGB
 (input CLK
-,output LED
+,output[2:0]LED
 );
 
 wire UP;
@@ -10,13 +10,28 @@ wire STT1;
 PWM up(CLK,1,STT0,UP);
 PWM dw(CLK,0,STT1,DW);
 
-reg[1:0]flag;
+reg[1:0]flag0=2'b00;
 always@(posedge STT0)
-	if(flag==2'b10)
-		flag<=1'b0;
+	if(flag0==2'b10)
+		flag0<=1'b0;
 	else
-		flag<=flag+1'b1;
+		flag0<=flag0+1'b1;
+assign LED[0]=(flag0==2'b00)?UP:((flag0==2'b01)?DW:1'b1);
 
-assign LED=(flag==2'b00)?UP:((flag==2'b01)?DW:1'b1);
+reg[1:0]flag1=2'b01;
+always@(posedge STT0)
+	if(flag1==2'b10)
+		flag1<=1'b0;
+	else
+		flag1<=flag1+1'b1;
+assign LED[1]=(flag1==2'b00)?UP:((flag1==2'b01)?DW:1'b1);
+
+reg[1:0]flag2=2'b10;
+always@(posedge STT0)
+	if(flag2==2'b10)
+		flag2<=1'b0;
+	else
+		flag2<=flag2+1'b1;
+assign LED[2]=(flag2==2'b00)?UP:((flag2==2'b01)?DW:1'b1);
 
 endmodule
